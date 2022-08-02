6aeba7b0e253dc367a9417d93b24b485ec3a20ce
D.M. Gihan Piumal Alwis <43994516+gihanpiumal@users.noreply.github.com>
2022-08-02 15:12:34 +0530

    Delete server directory

--- server/models/category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = new mongoose.Schema({
-  categoryName: { type: String, required: true },
-  Description: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Users", categorySchema);
